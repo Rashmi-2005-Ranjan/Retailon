b50cffabe883ff7d921073886c404fc3be7f4f24
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-07-24 19:34:14 +0530

    First Commit Backend

---
+++ retailon-backend/src/main/java/in/retailon/retailon_backend/RetailonBackendApplication.java
@@ -0,0 +1,13 @@
+package in.retailon.retailon_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailonBackendApplication.class, args);
+	}
+
+}
---
+++ retailon-backend/src/test/java/in/retailon/retailon_backend/RetailonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.retailon.retailon_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

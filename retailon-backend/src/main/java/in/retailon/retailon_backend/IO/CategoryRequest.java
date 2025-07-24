ad6fcdb4e89b811c69acbd7f541b9f2163066bbf
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-07-24 21:51:02 +0530

    Category Entity Request and Response Class

---
+++ retailon-backend/src/main/java/in/retailon/retailon_backend/IO/CategoryRequest.java
@@ -0,0 +1,15 @@
+package in.retailon.retailon_backend.IO;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class CategoryRequest {
+
+    private String name;
+    private String description;
+    private String bgColor;
+
+}

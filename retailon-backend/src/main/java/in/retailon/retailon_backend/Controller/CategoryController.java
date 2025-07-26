28498f0d6d00f6d7a2f587835e20e6c03eb858fb
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-07-26 21:56:43 +0530

    Solved CORS Issue From Frontend Using @Crossorigin and implement Object Mapper

--- retailon-backend/src/main/java/in/retailon/retailon_backend/Controller/CategoryController.java
+++ retailon-backend/src/main/java/in/retailon/retailon_backend/Controller/CategoryController.java
@@ -1,25 +1,36 @@
 package in.retailon.retailon_backend.Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import in.retailon.retailon_backend.IO.CategoryRequest;
 import in.retailon.retailon_backend.IO.CategoryResponse;
 import in.retailon.retailon_backend.Services.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class CategoryController {
     private final CategoryService categoryService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CategoryResponse addCategory(@RequestBody CategoryRequest request) {
-        return categoryService.add ( request );
+    public CategoryResponse addCategory(@RequestPart("category") String categoryString , @RequestPart("file") MultipartFile file) {
+        ObjectMapper objectMapper = new ObjectMapper ( );
+        CategoryRequest request = null;
+        try {
+            request = objectMapper.readValue ( categoryString , CategoryRequest.class );
+            return categoryService.add ( request , file );
+        } catch (JsonProcessingException e) {
+            throw new ResponseStatusException ( HttpStatus.BAD_REQUEST , "Exception Occurred While Parsing The JSON" + e.getMessage ( ) );
+        }
     }
 
     @GetMapping

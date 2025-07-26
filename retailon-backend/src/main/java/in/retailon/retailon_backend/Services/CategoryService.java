62d87f9912667b4a5f97734533f25de34ec23d61
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-07-26 21:58:56 +0530

    Added New abstract method in CategoryService and also added builder annotation in Category request

--- retailon-backend/src/main/java/in/retailon/retailon_backend/Services/CategoryService.java
+++ retailon-backend/src/main/java/in/retailon/retailon_backend/Services/CategoryService.java
@@ -3,12 +3,13 @@ package in.retailon.retailon_backend.Services;
 import in.retailon.retailon_backend.IO.CategoryRequest;
 import in.retailon.retailon_backend.IO.CategoryResponse;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Repository
 public interface CategoryService {
-    CategoryResponse add(CategoryRequest request);
+    CategoryResponse add(CategoryRequest request, MultipartFile file);
     List<CategoryResponse> read();
     void delete(String categoryId);
 }

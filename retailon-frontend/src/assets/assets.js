4015f74b47bb2f9090c31371851e5b489d45b50b
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-07-30 20:36:22 +0530

    assets file configuration

---
+++ retailon-frontend/src/assets/assets.js
@@ -0,0 +1,13 @@
+import logo from  './logo.png'
+import upload from './upload.png'
+import loginBg from './login-bg.jpg'
+import profile from './profile.png'
+import device from './device.png'
+
+export const assets={
+    logo,
+    upload,
+    loginBg,
+    profile,
+    device
+}

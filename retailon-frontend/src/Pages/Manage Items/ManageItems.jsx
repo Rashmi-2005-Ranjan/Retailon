e417f5b70827d8b2107724bf35320d1ab62ff26d
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-07-30 20:38:35 +0530

    Manage Item Component

---
+++ retailon-frontend/src/Pages/Manage Items/ManageItems.jsx
@@ -0,0 +1,19 @@
+import "./ManageItems.css"
+import ItemForm from "../../Components/Item Form/ItemForm.jsx";
+import ItemList from "../../Components/Item List/ItemList.jsx";
+
+const ManageItems = () => {
+    return (
+        <>
+            <div className="items-container text-light">
+                <div className="left-column">
+                    <ItemForm/>
+                </div>
+                <div className="right-column">
+                    <ItemList/>
+                </div>
+            </div>
+        </>
+    )
+}
+export default ManageItems

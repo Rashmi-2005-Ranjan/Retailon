7ca8ef9b91508260877f13ab9aefa8bf0d2d83b4
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-08-06 18:47:40 +0530

    Item List Component

--- retailon-frontend/src/Components/Item List/ItemList.jsx
+++ retailon-frontend/src/Components/Item List/ItemList.jsx
@@ -1,34 +1,36 @@
 import {useContext, useState} from "react";
-import {AppContext} from "../../Context/AppContext.jsx";
-import {deleteItem} from "../../Services/ItemService.js";
+import {AppContext} from "../../context/AppContext.jsx";
 import toast from "react-hot-toast";
 import './ItemList.css';
+import {deleteItem} from "../../Services/ItemService.js";
 
 const ItemList = () => {
     const {itemsData, setItemsData} = useContext(AppContext);
     const [searchTerm, setSearchTerm] = useState("");
+
     const filteredItems = itemsData.filter((item) => {
         return item.name.toLowerCase().includes(searchTerm.toLowerCase());
     })
+
     const removeItem = async (itemId) => {
         try {
             const response = await deleteItem(itemId);
             if (response.status === 204) {
                 const updatedItems = itemsData.filter(item => item.itemId !== itemId);
                 setItemsData(updatedItems);
-                toast.success("Item was deleted successfully.");
+                toast.success("Item deleted");
             } else {
-                console.error("Failed to delete item");
-                toast.error("Failed to delete item.");
+                toast.error("Unable to delete item");
             }
-        } catch (error) {
-            console.error("Error deleting item:", error);
-            toast.error("An error occurred while deleting the item.");
+        }catch(err) {
+            console.error(err);
+            toast.error("Unable to delete item");
         }
     }
+
     return (
-        <div className="category-list-container" style={{height: '100vh', overflowY: 'auto', overflowX: 'hidden'}}>
-            <div className="row pe-2">
+        <div className="category-list-container" style={{height:'100vh', overflowY: 'auto', overflowX: 'hidden'}}>
+            <div className="row">
                 <div className="input-group mb-3">
                     <input type="text"
                            name="keyword"
@@ -43,25 +45,25 @@ const ItemList = () => {
                     </span>
                 </div>
             </div>
-            <div className="row g-3 pe-2">
+            <div className="row g-3">
                 {filteredItems.map((item, index) => (
-                    <div className="col-12" key={index}>
-                        <div className="card p-3 item-card bg-dark">
+                    <div className="col-lg-12" key={index}>
+                        <div className="card p-3 bg-dark item-card">
                             <div className="d-flex align-items-center">
                                 <div style={{marginRight: '15px'}}>
-                                    <img src={item.imgUrl} alt={item.name} className="item-image"/>
+                                    <img src={item.imgUrl} alt={item.name} className="item-image" />
                                 </div>
                                 <div className="flex-grow-1">
                                     <h6 className="mb-1 text-white">{item.name}</h6>
                                     <p className="mb-0 text-white">
                                         Category: {item.categoryName}
                                     </p>
-                                    <span className="mb-0 text-black badge rounded-pill text-bg-warning">
-                                        &#8377; {item.price.toFixed(2)}
+                                    <span className="mb-0 text-block badge rounded-pill text-bg-warning">
+                                        &#8377;{item.price}
                                     </span>
                                 </div>
                                 <div>
-                                    <button className="btn btn-sm btn-danger" onClick={() => removeItem(item.itemId)}>
+                                    <button className="btn btn-danger btn-sm" onClick={() => removeItem(item.itemId)}>
                                         <i className="bi bi-trash"></i>
                                     </button>
                                 </div>

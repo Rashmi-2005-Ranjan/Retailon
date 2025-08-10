57c760a5a750cd47f3c0b039a62d12414ae080a7
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-08-10 11:23:51 +0530

    main file

--- retailon-backend/src/main/java/in/retailon/retailon_backend/RetailonBackendApplication.java
+++ retailon-backend/src/main/java/in/retailon/retailon_backend/RetailonBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RetailonBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RetailonBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run ( RetailonBackendApplication.class , args );
+    }
 
 }

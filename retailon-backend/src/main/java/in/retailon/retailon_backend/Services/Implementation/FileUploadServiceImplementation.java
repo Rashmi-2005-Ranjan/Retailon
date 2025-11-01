575e4dc0c0930bfd318c418514e10b92acfa1491
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-11-01 17:15:41 +0530

    Image uploading changed to cloudinary

--- retailon-backend/src/main/java/in/retailon/retailon_backend/Services/Implementation/FileUploadServiceImplementation.java
+++ retailon-backend/src/main/java/in/retailon/retailon_backend/Services/Implementation/FileUploadServiceImplementation.java
@@ -1,61 +1,65 @@
 package in.retailon.retailon_backend.Services.Implementation;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import in.retailon.retailon_backend.Services.FileUploadService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 import java.io.IOException;
-import java.util.UUID;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class FileUploadServiceImplementation implements FileUploadService {
-    @Value("${aws.bucket.name}")
-    private String bucketName;
 
-    private final S3Client s3Client;
+    private final Cloudinary cloudinary;
 
     @Override
     public String uploadFile(MultipartFile file) {
-        String filenameExtension = file.getOriginalFilename ( )
-                .substring ( file.getOriginalFilename ( ).lastIndexOf ( "." ) + 1 );
-        String key = UUID.randomUUID ( )
-                .toString ( ) + "." + filenameExtension;
         try {
-            PutObjectRequest putObjectRequest = PutObjectRequest.builder ( )
-                    .bucket ( bucketName )
-                    .key ( key )
-                    .acl ( "public-read" )
-                    .contentType ( file.getContentType ( ) )
-                    .build ( );
-            PutObjectResponse response = s3Client.putObject ( putObjectRequest , RequestBody.fromBytes ( file.getBytes ( ) ) );
-            if (response.sdkHttpResponse ( ).isSuccessful ( )) {
-                return "https://" + bucketName + ".s3.amazonaws.com/" + key;
-            } else {
-                throw new ResponseStatusException ( HttpStatus.INTERNAL_SERVER_ERROR , "An Error Occurred While Uploading The File" );
-            }
+            // Upload file to Cloudinary inside a folder named "retailon_images"
+            Map uploadResult = cloudinary.uploader().upload(
+                    file.getBytes(),
+                    ObjectUtils.asMap(
+                            "folder", "retailon_images",
+                            "resource_type", "auto" // handles images, videos, etc.
+                    )
+            );
+
+            // Return the Cloudinary secure URL (https)
+            return uploadResult.get("secure_url").toString();
+
         } catch (IOException e) {
-            throw new ResponseStatusException ( HttpStatus.INTERNAL_SERVER_ERROR , "An Error Occurred While Uploading The File" );
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    "An error occurred while uploading the file to Cloudinary",
+                    e
+            );
         }
     }
 
     @Override
     public boolean deleteFile(String imgUrl) {
-        String fileName = imgUrl.substring ( imgUrl.lastIndexOf ( "/" ) + 1 );
-        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder ( )
-                .bucket ( bucketName )
-                .key ( fileName )
-                .build ( );
-        s3Client.deleteObject ( deleteObjectRequest );
-        return true;
+        try {
+            // Extract public_id from the URL
+            String[] parts = imgUrl.split("/");
+            String fileName = parts[parts.length - 1];
+            String publicId = "retailon_images/" + fileName.substring(0, fileName.lastIndexOf('.'));
+
+            // Delete from Cloudinary
+            cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+            return true;
+
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    "An error occurred while deleting the file from Cloudinary",
+                    e
+            );
+        }
     }
 }
